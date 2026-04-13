73e058d2a4a242e025c7ff0bf0070b55cbdf8491
Rin <nguyenduyminhhoang@gmail.com>
2026-04-13 17:59:56 +0700

    chore: remove unused check_len.py script

--- check_len.py
+++
@@ -1,8 +0,0 @@
-import csv, glob
-names = []
-for p in glob.glob("data/thutuchanhchinh/TTHC_IDs/*/id_tthc.csv"):
-    with open(p, 'r') as f:
-        for r in csv.DictReader(f):
-            names.append(r['PROCEDURE_NAME'])
-names.sort(key=len)
-print(names[:10])
